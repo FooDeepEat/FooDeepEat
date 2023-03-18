4abcfa0cdb6a668f923a69f1a7c1d05060cba6f1
seokcode <ssd3784@gmail.com>
2023-03-19 01:10:39 +0900

    음식 모델 어드민 추가

--- calorie/admin.py
+++ calorie/admin.py
@@ -3,4 +3,7 @@ from . import models
 
 # Register your models here.
 
-# admin.site.register(models.FoodImage)
+admin.site.register(models.Food)
+admin.site.register(models.FoodImage)
+admin.site.register(models.UserFood)
+admin.site.register(models.UserMemo)
