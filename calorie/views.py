f3f54859719ec50a8ae5c35d4f2caf99588edcb2
seokcode <ssd3784@gmail.com>
2023-03-19 17:23:20 +0900

    메모장 기능 구현에 따른 수정

--- calorie/views.py
+++ calorie/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render, redirect
-from datetime import datetime
+from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from . import models
+
+
+@login_required
+def mypage(request, date=None):
+    user = request.user
+    date = request.GET.get('date') or timezone.localdate().strftime("%Y-%m-%d")
+
+    if request.method == 'POST':
+        memo_text = request.POST.get('memo_text', '')
+
+        if memo_text:
+            memos, created = models.UserMemo.objects.get_or_create(user_id=user.id, date=date,
+                                                                   defaults={'description': memo_text})
+            if not created:
+                memos.description = memo_text
+                memos.save()
+        else:
+            memos = models.UserMemo.objects.filter(user_id=user.id, date=date).first()
+            if memos:
+                memos.delete()
+        redirect('mypage_with_date', date=date)
+
+    memos = models.UserMemo.objects.filter(user_id=user.id, date=date).first()
+    foods = models.UserFood.objects.filter(user_id=user.id, date=date).first()
+    return render(request, "mypage.html", {"user": user, "memos": memos, "date": date, "foods": foods})
 
 
 @login_required
@@ -11,43 +36,9 @@ def service(request):
     return render(request, "service.html", {"user": user})
 
 
-@login_required
-def mypage(request, date=None):
-    user = request.user
-
-    if not date:
-        today = datetime.now().date().strftime("%Y-%m-%d")
-        # URL 패턴 이름으로부터 URL 경로를 만들어주는 함수: reverse
-        return redirect(reverse('mypage_with_date', kwargs={'date': today}))
-
-    elif request.method == 'POST':
-        edit_date = request.POST.get('edit_date')
-        # print(edit_date)
-        if not edit_date:
-            edit_date = datetime.now().date().strftime("%Y-%m-%d")
-        memo_text = request.POST.get('memo_text', '')
-        # print(edit_date, memo_text)
-        if memo_text:
-            memos, created = models.UserMemo.objects.get_or_create(user=user, date=edit_date,
-                                                                   defaults={'description': memo_text})
-            if not created:
-                memos.description = memo_text
-                memos.save()
-        else:
-            memos = models.UserMemo.objects.filter(user_id=user.id, date=edit_date).first()
-            if not memos:
-                models.UserMemo.objects.filter(user=user, date=edit_date).delete()
-        return redirect('mypage_with_date', date=edit_date)
-
-    memos = models.UserMemo.objects.filter(user_id=user.id, date=date).first()
-    foods = models.UserFood.objects.filter(user_id=user.id, date=date).first()
-    return render(request, "mypage.html", {"user": user, "memos": memos, "date": date, "foods": foods})
-
-
 def home(request):
     return render(request, "home.html")
 
 
 def introduce(request):
     return render(request, "introduce.html")
-
