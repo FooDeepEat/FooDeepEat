75d8da2c5ca3d96ef250c6925d534195d51264ed
ming <ming2natanatda@gmail.com>
2023-03-23 21:16:36 +0900

    회원가입,홈 등등등,,,

---
+++ calorie/static/js/home.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+	navigation: {
+		prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next',
+	  },
+	//   autoplay: {
+	// 	delay: 1500,
+	// 	disableOnInteraction: false,
+	//   },
+	  slidesPerView: 1, // 보여지는 슬라이드 수
+	  spaceBetween: 30, // 슬라이드 간의 거리(px 단위)
+	  loop: true, // 슬라이드 무한 반복
+	  touchRatio: 0,//드래그 금지
+ });
+
